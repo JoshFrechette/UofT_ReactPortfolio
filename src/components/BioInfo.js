c1e3dcb93dbab714d484d366331e26976ed50264
Josh Frechette <josh.f.design@gmail.com>
2020-02-27 19:42:02 -0500

    Saving progress on Deploy fix.

---
+++ src/components/BioInfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class BioInfo extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+// function bioInfo
+
+// export default BioInfo;v
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import BioInfo from "./components/BioInfo";
-import PortfolioInfo from "./components/PortfolioInfo";
-import ContactInfo from "./components/ContactInfo";
-// import "../styles/Main.css";
+// import React, { Component } from "react";
+// import BioInfo from "./components/BioInfo";
+// import PortfolioInfo from "./components/PortfolioInfo";
+// import ContactInfo from "./components/ContactInfo";
+// // import "../styles/Main.css";
 
-export default class Main extends Component {
-  render() {
-    return (
-      <>
-        <BioInfo />
-        <PortfolioInfo />
-        <ContactInfo />
-      </>
-    );
-  }
-}
+// export default class Main extends Component {
+//   render() {
+//     return (
+//       <>
+//         <BioInfo />
+//         <PortfolioInfo />
+//         <ContactInfo />
+//       </>
+//     );
+//   }
+// }

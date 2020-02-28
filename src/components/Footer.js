cbc42c3c63fa8627aa0f70f127ce30ad97c64799
Josh Frechette <josh.f.design@gmail.com>
2020-02-28 11:51:30 -0500

    Reboot; Components working.

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import "../styles/Footer.css";
+
+export default class Footer extends Component {
+    render() {
+      return (
+        <div className="footer">
+         <span class="text-muted">Copyright Joshua Fr&eacute;chette Design &copy;</span>
+        </div>
+      )
+    }
+  }

016383e742fdbf729acf50444382eb0adc135583
Josh Frechette <josh.f.design@gmail.com>
2020-03-26 16:50:44 -0400

    Styled the content.

--- src/components/BioInfo.js
+++ src/components/BioInfo.js
@@ -6,25 +6,25 @@ export default class BioInfo extends Component {
     render() {
         return (
             <div className="bio">
-            <h2 id="about-me">About Me</h2>
-            <hr></hr>
-            <img src={BioPic} className="profile" alt="Professional profile of Josh Frechette"></img>
-            <h3>Josh Fr&eacute;chette</h3>
-            <p>
-            Web Developer with a certificate in Full Stack Web Development from the University of Toronto. Experience as a Graphic Designer 
-            and gained a bachelor's degree in Graphic Design at the Ontarion College of Art &amp; Design. Background in the hospitality industry, 
-            including supervising and management roles. Design software skills in Adobe Creative Suite and Coding skills in HTML, CSS, and Javascript.
+                <h2 id="about-me">About Me</h2>
+                <hr></hr>
+                <img src={BioPic} className="profile" alt="Professional profile of Josh Frechette"></img>
+                <h3>Josh Fr&eacute;chette</h3>
+                <p>
+                    Front End Developer with a certificate in Full Stack Web Development from the University of Toronto. Experience as a Graphic Designer
+                    and gained a bachelor's degree in Graphic Design at the Ontarion College of Art &amp; Design. Background in the hospitality industry,
+                    including supervising and management roles. Design software skills in Adobe Creative Suite and Coding skills in HTML, CSS, and Javascript.
             </p>
-    
-            <p>
-            Strong abilities in teamwork, organization, and handling stress with composure. I enjoy applying new web capabilities to practical applications.
-            Passionate about successfully taking a project from concept to actualization.
+
+                <p>
+                    Strong abilities in teamwork, organization, and handling stress with composure. I enjoy applying new web capabilities to practical applications.
+                    Passionate about successfully taking a project from concept to actualization.
             </p>
-    
-            <p>
-            Using my combined expertise to create an immersive user experience, I'm excited to join a team of like-minded individuals who are striving to leave their mark on the web.
+
+                <p>
+                    Using my combined expertise to create an immersive user experience, I'm excited to join a team of like-minded individuals who are striving to leave their mark on the web.
             </p>
-            <i className="far fa-file-pdf"></i> <a className="resume" href="Assets/JoshuaFrechette_Resume_2019.pdf" target="_blank">View Resume</a>
+                <i className="far fa-file-pdf"></i> <a className="resume" href="Assets/JoshuaFrechette_Resume_2019.pdf" target="_blank">View Resume</a>
             </div>
         )
     }

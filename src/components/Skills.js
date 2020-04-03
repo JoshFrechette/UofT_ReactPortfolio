21e2d950c02fe0dcdb9d8a0ce53a44d40e5e34ed
Josh Frechette <josh.f.design@gmail.com>
2020-04-03 15:17:02 -0400

    Added a skills section.

---
+++ src/components/Skills.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../styles/Skills.css";
+import reactLogo from "../img/logo192.png";
+import nodeLogo from "../img/nodejs.png";
+import htmlLogo from "../img/HTML5_Badge_128.png";
+import mongoLogo from "../img/MongoDB_Logo_FullColorBlack_RGB.png";
+import jsLogo from "../img/240px-JavaScript-logo.png";
+
+let Skills = () => {
+
+    return (
+        <div className="container skills">
+            <div className="row">
+                <h2 id="skills">Skills</h2>
+                <hr></hr>
+            </div>
+            <div className="row skillview">
+                <div className="col"><img src= {reactLogo} alt="react logo"></img></div>
+                <div className="col"><img src= {nodeLogo} alt="node.js logo"></img></div>
+                <div className="col"><img src= {htmlLogo} alt="html5 logo"></img></div>
+                <div className="col"><img src= {mongoLogo} alt="mongoDB logo"></img></div>
+                <div className="col"><img src= {jsLogo} alt="javascript logo"></img></div>
+            </div>
+        </div>
+    )
+}
+
+export default Skills;
--- src/views/Main.js
+++ src/views/Main.js
@@ -3,19 +3,21 @@ import BioInfo from "../components/BioInfo";
 import PortfolioInfo from "../components/PortfolioInfo";
 import ContactInfo from "../components/ContactInfo";
 import Greeting from "../components/Greeting";
+import Skills from "../components/Skills";
 
 import "../styles/Main.css";
 
 let Main = () => {
 
-      return (
-        <div className="main">
-            <Greeting />
-            <BioInfo />
-            <PortfolioInfo />
-            <ContactInfo />
-        </div>
-      )
-      }
+  return (
+    <div className="main">
+      <Greeting />
+      <BioInfo />
+      <Skills />
+      <PortfolioInfo />
+      <ContactInfo />
+    </div>
+  )
+}
 
-  export default Main;
+export default Main;

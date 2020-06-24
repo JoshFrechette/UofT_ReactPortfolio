1b475ecdd1d953e9d6723030d8a192588fe8cda0
Josh Frechette <josh.f.design@gmail.com>
2020-06-23 21:37:10 -0400

    Updated FWT? formatting.

--- src/techPages/Burger.js
+++ src/techPages/Burger.js
@@ -6,26 +6,20 @@ import github from "../img/GitHub-Mark-Light-32px.png";
 
 let Burger = () => {
     return (
-        <div className="container specs">
-            <div className="row project">
-                <div className="col">
-                        <h1>Fries With That?</h1>
-                        <p></p>
+        <div className="grid-x grid-margin-x small-margin-collapse align-center">
+            <div className="cell small-12 medium-8 project">
+                <div className="techhead">
+                    <h1>Fries With That?</h1>
+                    <p></p>
+                    <a href='https://joshfdesign-fries-with-that.herokuapp.com/' target="_blank" rel="noopener noreferrer"><button className="button"><img className="buttonlogo" src={heroku} alt="heroku logo"></img>Heroku Deployment</button></a>
+                    <a href='https://github.com/JoshFrechette/Fries_With_That.git' target="_blank" rel="noopener noreferrer"><button className="button"><img className="buttonlogo" src={github} alt="github logo"></img>GitHub Repository</button></a>
+
                 </div>
-            <div className="row buttons">
-                        <div className="col small-4">
-                            <a href='https://joshfdesign-fries-with-that.herokuapp.com/' target="_blank" rel="noopener noreferrer"><button className="button"><img className="buttonlogo" src={heroku} alt="heroku logo"></img>Heroku Deployment</button></a>
-                        </div>
-                        <div className="col small-4">
-                            <a href='https://github.com/JoshFrechette/Fries_With_That.git' target="_blank" rel="noopener noreferrer"><button className="button"><img className="buttonlogo" src={github} alt="github logo"></img>GitHub Repository</button></a>
-                        </div>
-            </div>
-            <div className="row">
-                <div className="col-lg-6 img">
+                <div className="img">
                     <img className="preview" src={Preview} alt="gif of the app" />
                 </div>
-            </div>
-                <div>
+
+                <div className="techbody">
                     <h3>Technical Sheet</h3>
                     <p>Solo project, with an emphasis on ORMs, CRUDs, mySQL and Handlebars. The application allows the user to enter any choice of burger and change it's status when it has been eaten.</p>
                     <hr></hr>
@@ -36,7 +30,7 @@ let Burger = () => {
 
                 <div>
                     <h3>Technologies Used</h3>
-                    <p>Code technologies and skills I was involved with using to complete the project.</p>
+                    <p>Code technologies and skills I was involved with to complete the project.</p>
                     <ul className="tech">
                         <li>HTML5</li>
                         <li>CSS3</li>
--- src/techPages/PostUp.js
+++ src/techPages/PostUp.js
@@ -7,50 +7,49 @@ import github from "../img/GitHub-Mark-Light-32px.png";
 let PostUp = () => {
     return (
         <div className="grid-x grid-margin-x small-margin-collapse align-center">
-             {/* <div className="grid project align-center"> */}
-                <div className="cell small-12 medium-8 project">
-                    <div className="techhead">
-                        <h1>PostUp Cards</h1>
-                        <p></p>
-                        <a href='https://postup-cards.herokuapp.com/' target="_blank" rel="noopener noreferrer"><button className="button"><img className="buttonlogo" src={heroku} alt="heroku logo"></img>Heroku Deployment</button></a>
-                        <a href='https://github.com/JoshFrechette/PostUp-Cards.git' target="_blank" rel="noopener noreferrer"><button className="button"><img className="buttonlogo" src={github} alt="github logo"></img>GitHub Repository</button></a>
-                    </div>
-                    <div className="img">
-                        <img className="preview" src={Preview} alt="gif of PostUp Cards" />
-                    </div>
-
-                    <div>
-                        <h3>Technical Sheet</h3>
-                        <p>I was part of a 4 person team of developers tasked with developing an original web application using HandleBars to render content and MySQL to connect to the database.</p>
-                        <hr></hr>
-                        <p> My initial role in project was project manager and I also had the responsibility of setting up the routes and creating the schema for the MySQL database. I eventually
-                            became more hands-on in the overall design of the app and implementing the javascript into the app. This project helped push me out of my comfort zone of working on front-end design and
-                            into more challenging back-end content management. 
-                        </p>
-                    </div>
-
-                    <div>
-                        <h3>Technologies Used</h3>
-                        <p>Code technologies and skills I was involved with using to complete the project.</p>
-                        <ul className="tech">
-                            <li>HTML5</li>
-                            <li>CSS3</li>
-                            <li>Javascript</li>
-                            <li>Foundation CSS</li>
-                            <li>Handlebars</li>
-                            <li>Node.js</li>
-                            <li>Express.js</li>
-                            <li>MySQL</li>
-                        </ul>
-                        <p>APIs used</p>
-                        <ul className="tech">
-                            <li><a className="links" href="https://www.balldontlie.io/#introduction" target="_blank" rel="noopener noreferrer">Ball Don't Lie</a></li>
-                            <li><a className="links" href="https://developers.giphy.com/" target="_blank" rel="noopener noreferrer">Giphy</a></li>
-                        </ul>
-                    </div>
+            <div className="cell small-12 medium-8 project">
+                <div className="techhead">
+                    <h1>PostUp Cards</h1>
+                    <p></p>
+                    <a href='https://postup-cards.herokuapp.com/' target="_blank" rel="noopener noreferrer"><button className="button"><img className="buttonlogo" src={heroku} alt="heroku logo"></img>Heroku Deployment</button></a>
+                    <a href='https://github.com/JoshFrechette/PostUp-Cards.git' target="_blank" rel="noopener noreferrer"><button className="button"><img className="buttonlogo" src={github} alt="github logo"></img>GitHub Repository</button></a>
                 </div>
+                <div className="img">
+                    <img className="preview" src={Preview} alt="gif of PostUp Cards" />
+                </div>
+
+                <div className="techbody">
+                    <h3>Technical Sheet</h3>
+                    <p>I was part of a 4 person team of developers tasked with developing an original web application using HandleBars to render content and MySQL to connect to the database.</p>
+                    <hr></hr>
+                    <p> My initial role in project was project manager and I also had the responsibility of setting up the routes and creating the schema for the MySQL database. I eventually
+                    became more hands-on in the overall design of the app and implementing the javascript into the app. This project helped push me out of my comfort zone of working on front-end design and
+                    into more challenging back-end content management.
+                        </p>
+                </div>
+
+                <div>
+                    <h3>Technologies Used</h3>
+                    <p>Code technologies and skills I was involved with to complete the project.</p>
+                    <ul className="tech">
+                        <li>HTML5</li>
+                        <li>CSS3</li>
+                        <li>Javascript</li>
+                        <li>Foundation CSS</li>
+                        <li>Handlebars</li>
+                        <li>Node.js</li>
+                        <li>Express.js</li>
+                        <li>MySQL</li>
+                    </ul>
+                    <p>APIs used</p>
+                    <ul className="tech">
+                        <li><a className="links" href="https://www.balldontlie.io/#introduction" target="_blank" rel="noopener noreferrer">Ball Don't Lie</a></li>
+                        <li><a className="links" href="https://developers.giphy.com/" target="_blank" rel="noopener noreferrer">Giphy</a></li>
+                    </ul>
+                </div>
+            </div>
             {/* </div> */}
-         </div>
+        </div>
     )
 }
 
